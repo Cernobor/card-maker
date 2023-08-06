57386ddb0b9a8a6a15a39230edb8ab16bc15831c
Jakub <jakub.rutrle@firma.seznam.cz>
2023-08-07 00:29:18 +0200

    basic template rendering

--- card_maker/web/routes.py
+++ card_maker/web/routes.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from time import sleep
 
 app = Flask(__name__)
 
 
-@app.route("/aspekt", methods=["GET"])
+@app.route("/aspekt", methods=["GET","POST"])
 def vytvor_karticku_aspektu(jmeno_aspektu: str = "aspekt", text_aspektu: str = "foo", fluff: str = "bar"):
-    sleep(10)
-    return {"jmeno_aspektu": jmeno_aspektu, "text_aspektu": text_aspektu, "fluff": fluff}
+
+    return render_template("aspekt.html")
