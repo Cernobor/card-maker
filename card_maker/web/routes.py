a34e03ab09af93a5f7375d856cf46241c7e2ed99
Jakub <jakub.rutrle@firma.seznam.cz>
2023-08-08 22:32:33 +0200

    another download tryout

--- card_maker/web/routes.py
+++ card_maker/web/routes.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, send_file
 from time import sleep
+from card_maker.app.card_maker import create_magical_item
 
 app = Flask(__name__)
 
 
 @app.route("/aspekt", methods=["GET","POST"])
-def vytvor_karticku_aspektu(jmeno_aspektu: str = "aspekt", text_aspektu: str = "foo", fluff: str = "bar"):
+def create_aspect_card(jmeno_aspektu: str = "aspekt", text_aspektu: str = "foo", fluff: str = "bar"):
 
     return send_file(r"/home/jakub/Desktop/code_main/card-maker-api/card_maker/web/kapy.png", mimetype="image/png", as_attachment=True)
