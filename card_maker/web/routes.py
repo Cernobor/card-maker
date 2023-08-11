cfc9f83184f1fa34f799045caf435ce1473b4a74
Jakub Rutrle <rutrle@seznam.cz>
2023-08-11 17:17:12 +0200

    added card preview

--- card_maker/web/routes.py
+++ card_maker/web/routes.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, send_file, flash, url_for, after_this_request
-from time import sleep
+from flask import Flask, render_template, send_file, flash, url_for, after_this_request, request
 from card_maker.app.card_maker import create_free_aspect
 from card_maker.web.forms import AspektForm
 import shutil
@@ -17,11 +16,34 @@ def test_base():
 def aspekt_create():
     form = AspektForm()
 
-    img_basename = "default.png"
-    
-    
+    clean_cards_directory()
 
-    if form.validate_on_submit():
+    if form.validate_on_submit() and request.form['action'] == 'Vytvořit & stáhnout':
+        name = form.name.data
+        effect = form.effect.data
+        frame = form.frame.data
+        fluff = form.fluff.data
+        activation = form.activation.data
+        inactivation = form.inactivation.data
+        additional_effect = form.additional_effect.data
+        additional_effect = list(filter(None, additional_effect))
+
+        aspekt_path = create_free_aspect(name, effect, frame, fluff, activation, inactivation, additional_effect)
+
+        new_path = os.path.join(os.path.dirname(__file__),"static","created_cards", os.path.basename(aspekt_path))
+        shutil.move(aspekt_path, new_path)
+
+        @after_this_request
+        def delete_file(response):
+            try:
+                os.remove(new_path)
+            except Exception as error:
+                app.logger.error("Error removing or closing downloaded file handle", error)
+            return response
+
+        return send_file(new_path, mimetype="image/png", as_attachment=True)
+
+    elif form.validate_on_submit() and request.form['action'] == 'Náhled':
         name = form.name.data
         effect = form.effect.data
         frame = form.frame.data
@@ -38,26 +60,24 @@ def aspekt_create():
 
         img_basename = os.path.basename(aspekt_path)
 
-        @after_this_request
-        def delete_file(response):
-            try:
-                os.remove(new_path)
-            except Exception as error:
-                app.logger.error("Error removing or closing downloaded file handle", error)
-            return response
-
-        return send_file(new_path, mimetype="image/png", as_attachment=True)
-
-
+        return render_template("aspekt.html", form=form, img_source=img_basename)
+    
     if form.errors != {}:
         for err_msg in form.errors.values():
             flash(f'Vyskytl se problém: {err_msg[0]}', category='danger', )
     
 
     
-    return render_template("aspekt.html", form=form, img_source=img_basename)
+    return render_template("aspekt.html", form=form)
     
 @app.route("/preview", methods=["GET"])
 def card_preview():
     img_source = r"/home/jakub-rutrle/code_main/card-maker/card_maker/web/static/default.png"
     return render_template("preview.html", img_source=img_source)
+
+
+def clean_cards_directory():
+    directory = os.path.join(os.path.dirname(__file__),"static","created_cards")
+    for file in os.listdir(directory):
+        if file.endswith(".png"):
+            os.remove(os.path.join(directory, file))
