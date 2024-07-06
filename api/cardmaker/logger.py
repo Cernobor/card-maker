0d12571a3ac14ae30d81b6e3cb7ef82a7c7295d9
pulcovamon <pulcovamon@gmail.com>
2024-07-06 23:33:24 +0200

    add some whitespaces and docstring to logger

--- api/cardmaker/logger.py
+++ api/cardmaker/logger.py
@@ -7,11 +7,15 @@ import sys
 
 
 class Logger(object):
-
+    """
+    Logger singleton class
+    """
     _instance = None
+
 
     def __init__(self):
         raise RuntimeError("Call get_instance() instead")
+
 
     @classmethod
     def get_instance(cls):
@@ -20,22 +24,27 @@ class Logger(object):
             cls._instance.setup_logger()
         return cls._instance
 
+
     def debug(self, message):
         self.logger.debug(message)
+
 
     def info(self, message):
         self.logger.info(message)
 
+
     def warning(self, message):
         self.logger.warning(message)
+
 
     def error(self, message):
         self.logger.error(message)
 
+
     def setup_logger(self):
         """
         Setup for python logger.
-        All messages are logged into file and stdout.
+        All messages are logged into file 'cardmaker-api.log' and stdout.
         """
         formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 
@@ -46,6 +55,7 @@ class Logger(object):
         stream_handler.setFormatter(formatter)
 
         self.logger = logging.getLogger("CardmakerApi")
+        # it shall be changed to info or warning before deploy
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(file_handler)
         self.logger.addHandler(stream_handler)
