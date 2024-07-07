6dd59288eeceda96849b0558178102051bc11696
Jakub <jakub.rutrle@firma.seznam.cz>
2024-07-07 19:18:33 +0200

    Add card load from data

--- cardMakerFE/src/routes/card/[id]/+page.svelte
+++ cardMakerFE/src/routes/card/[id]/+page.svelte
@@ -1,48 +1,39 @@
 <script>
-    import { PUBLIC_BASE_API_URL } from '$env/static/public';
-    /** @type {import('./$types').PageData} */
-    export let data
-    let mode = 'update'
+	import { PUBLIC_BASE_API_URL } from '$env/static/public';
+	/** @type {import('./$types').PageData} */
+	export let data;
+	let mode = 'update';
 
 	import Card from '$lib/Card.svelte';
 	import CardForm from '$lib/CardForm.svelte';
-    
+
 	let cardTypes = ['Magický předmět', 'Volný aspekt', 'Lokace'];
 
-    async function getCard() {
-        try {
-            console.log( data.card_id)
-            const response = await fetch(PUBLIC_BASE_API_URL + '/cardmaker/cards/' + data.card_id);
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-            const json = await response.json();
-            return json;
-        } catch (error) {
-            console.error(error.message);
-    }}
-    let mycard = getCard();
+	async function getCard() {
+		try {
+			console.log(data.card_id);
+			const response = await fetch(PUBLIC_BASE_API_URL + '/cardmaker/cards/' + data.card_id);
+			if (!response.ok) {
+				throw new Error(`Response status: ${response.status}`);
+			}
+			const json = await response.json();
+			return json;
+		} catch (error) {
+			console.error(error.message);
+		}
+	}
+	let card = {};
 
-
-
-
-	let card = {
-        id:data.card_id,
-		name: 'Card Name',
-		type: cardTypes[0],
-		fluff: 'Card Fluff',
-		effect: 'Efekt/pravidla karty',
-		nonRemovable: true,
-		inSet: false,
-		setName: 'Jméno setu (počet itemů v setu)',
-
-		tags: []
-	};
-
+	async function loadCard() {
+		const cardData = await getCard();
+		console.log(cardData);
+		card = cardData;
+	}
+	loadCard();
 	let cardComponent;
 </script>
 
-<h1>update card of id { data.card_id }</h1>
+<h1>update card of id {data.card_id}</h1>
 
 <div class="cardmaker-body">
 	<div class="inputs">
