cf2c2b164228a7782d59f09068d3bd795fab4250
pulcovamon <pulcovamon@gmail.com>
2024-07-07 17:07:35 +0200

    create svelte component for dropdown menu

---
+++ cardMakerFE/src/lib/FilterDropdown.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { Author, CardType } from "$lib/types";
+    export let options: Author[]|CardType[]|[];
+    export let selected: Author|CardType|null;
+    export let filterName: string;
+</script>
+
+<div>
+    <h3>{filterName}</h3>
+    <select bind:value={selected}>
+        <option value={null}>All</option>
+        {#each options as optionValue}
+            <option value={optionValue}>{optionValue.name}</option>
+        {/each}
+    </select>
+</div>
