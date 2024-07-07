b22e68c385f5ceeef06a7556971a47ae5c332b9a
pulcovamon <pulcovamon@gmail.com>
2024-07-07 17:05:52 +0200

    define sone custom types

---
+++ cardMakerFE/src/lib/types.ts
@@ -0,0 +1,13 @@
+export type Author = {
+    name: string;
+    id: number;
+};
+
+export type CardType = {
+    name: string;
+    id: number;
+};
+
+export type ErrorWithMessage = {
+    message: string;
+}
