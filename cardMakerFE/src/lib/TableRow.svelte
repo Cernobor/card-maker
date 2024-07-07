26a0bdfcc6d7c58d9dcac59a0c08d8d1a18ddef7
pulcovamon <pulcovamon@gmail.com>
2024-07-07 17:04:59 +0200

    cook table component

---
+++ cardMakerFE/src/lib/TableRow.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let cardName: string;
+    export let cardID: number;
+    export let cardAuthor: string;
+    export let cardType: string;
+</script>
+
+<tr>
+    <a href="/card/{cardID}">
+        <td><b>{cardName}</b></td>
+        <td>{cardAuthor}</td>
+        <td>{cardType}</td>
+    </a>
+</tr>
