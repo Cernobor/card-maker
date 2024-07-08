e8d887dda7f2e51871fc7ac89ea2801cd8df3285
zegkljan <zegkljan@gmail.com>
2024-07-09 01:42:06 +0200

    Serving api and client from server.
    * Server serves the API under the /api prefix.
    * Server serves the client from directory specified by CLIENT_FILES env var (or /client by default) under the / prefix.

--- api/main.py
+++ api/main.py
@@ -2,15 +2,32 @@
 App for creating summer camp cards and saving them in database.
 """
 
+import os
 import uvicorn
 from cardmaker import endpoints
 from create_db import create_db
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope):
+        try:
+            return await super().get_response(path, scope)
+        except (HTTPException, StarletteHTTPException) as ex:
+            if ex.status_code == 404:
+                return await super().get_response("index.html", scope)
+            else:
+                raise ex
+
+
+api = FastAPI(title="api")
+api.include_router(endpoints.router)
+
+client_files = SPAStaticFiles(directory=os.getenv("CLIENT_FILES", "/client"))
 
 app = FastAPI(title="cardmaker")
-app.include_router(endpoints.router, prefix="/cardmaker")
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -18,6 +35,8 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
+app.mount("/api", api)
+app.mount("/", client_files)
 
 if __name__ == "__main__":
     create_db()
