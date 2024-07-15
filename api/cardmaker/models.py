6d9ebd4020fc18ba83735f7f974122e2d9d72633
pulcovamon <pulcovamon@gmail.com>
2024-07-15 23:35:58 +0200

    create models for post and get methods, add password to user

--- api/cardmaker/models.py
+++ api/cardmaker/models.py
@@ -8,16 +8,36 @@ from typing import Optional
 from sqlmodel import Field, Relationship, SQLModel
 
 
-class User(SQLModel, table=True):
+class UserBase(SQLModel):
     """
-    Author of the card.
+    Base model of author of the card.
+    """
+    username: str
+
+
+class UserCreate(UserBase):
+    """
+    User model for 'create_user' POST method.
+    """
+    password: str
+
+
+class UserPublic(UserBase):
+    """
+    User model for 'get_users' GET method.
+    """
+    id: int
+
+
+class User(UserBase, table=True):
+    """
     Each Card instance has foreign key to User
     """
-
     __tablename__ = "users"
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    hashed_password: Optional[str]
+    anonymous: bool
 
 
 class CardType(SQLModel, table=True):
@@ -25,7 +45,6 @@ class CardType(SQLModel, table=True):
     Type of card, which means for example Location or Magical item.
     Each Card has foreign key to CardType.
     """
-
     __tablename__ = "card_types"
 
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -36,47 +55,64 @@ class CardTagRelationship(SQLModel, table=True):
     """
     Link model for Card and Tag models.
     """
-
     __tablename__ = "cards_tags_relationship"
 
     card_id: int = Field(default=None, foreign_key="cards.id", primary_key=True)
     tag_id: int = Field(default=None, foreign_key="tags.id", primary_key=True)
 
 
-class Card(SQLModel, table=True):
+class CardBase(SQLModel):
     """
-    Main item of the app.
-    Each Card instance has author (User) and type (CardType).
+    Base model for card: main item of the app.
     """
-
-    __tablename__ = "cards"
-
-    id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     fluff: Optional[str]
     effect: Optional[str]
-    user_id: int = Field(foreign_key="users.id")
-    card_type_id: int = Field(foreign_key="card_types.id")
     in_set: bool
     set_name: Optional[str]
+
+
+class CardCreate(CardBase):
+    """
+    Card model for 'create_card' POST method.
+    """
+    tags: List["TagBase" | None]
+    user_id: int
+    card_type_id: int
+
+
+class Card(SQLModel, table=True):
+    """
+    Each Card instance has author (User) and type (CardType).
+    """
+    __tablename__ = "cards"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="users.id")
+    card_type_id: int = Field(foreign_key="card_types.id")
 
     tags: list["Tag"] = Relationship(
         back_populates="cards", link_model=CardTagRelationship
     )
 
 
-class Tag(SQLModel, table=True):
+class TagBase(SQLModel):
     """
-    Tag, which can be attached to any Card instance.
+    Base model for tag.
     Tag can be anything, for example year of creation
     of something, which can help with filtering.
     """
+    name: str
+    description: Optional[str]
 
+
+class Tag(TagBase, table=True):
+    """
+    Tag, which can be attached to any Card instance.
+    """
     __tablename__ = "tags"
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    description: Optional[str]
 
     cards: list[Card] = Relationship(
         back_populates="tags", link_model=CardTagRelationship
