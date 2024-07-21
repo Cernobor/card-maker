01d1ec8f97f0b7417067e911673a96052e2fda73
pulcovamon <pulcovamon@gmail.com>
2024-07-21 20:26:03 +0200

    start with creating login on frontend

---
+++ cardMakerFE/src/routes/login/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { User } from "$lib/types";
+    let user: User;
+
+    function handleSubmit(e) {
+        try {
+            /*
+             * TODO: saveUserOrError
+             */
+        } catch {
+            e.prevendDefalt()
+            user.username = "";
+            user.password = "";
+            alert("Nesprávné uživatelské jméno nebo heslo.")
+        }
+    }
+
+</script>
+
+<div>
+    <form on:submit={handleSubmit}>
+        <label for="username">Uživatelské jméno</label>
+        <input type="text" bind:value={user.username}/>
+        <label for="password">Heslo</label>
+        <input type="hidden" bind:value={user.password} />
+        <input type="submit" value="Přihlásit se" />
+    </form>
+</div>
---
+++ cardMakerFE/src/routes/registration/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import type { User } from "$lib/types";
+    let user: User;
+    let passwordConfirm: string;
+
+
+    function wrongRegistration(e, alertText: string) {
+        e.prevendDefalt()
+        user.username = "";
+        user.password = "";
+        passwordConfirm = "";
+        alert(alertText)
+    }
+
+    function handleSubmit(e) {
+        if (user.password != passwordConfirm) {
+            wrongRegistration(e, "Heslo se musí shodovat!")
+        }
+        try {
+            /*
+             * TODO: saveUserOrError
+             */
+        } catch {
+            wrongRegistration(e, `Uživatel ${user.username} už existuje.`)
+        }
+    }
+
+</script>
+
+<div>
+    <form on:submit={handleSubmit}>
+        <label for="username">Uživatelské jméno</label>
+        <input type="text" bind:value={user.username}/>
+        <label for="password">Heslo</label>
+        <input type="hidden" bind:value={user.password} />
+        <label for="passwordConfirm">Potvrdit heslo</label>
+        <input type="hidden" bind:value={passwordConfirm} />
+        <input type="submit" value="Registrovat se" />
+    </form>
+</div>
