945f0e452c7371c273507cf9edfae2481a7bc207
pulcovamon <pulcovamon@gmail.com>
2024-07-21 22:20:23 +0200

    fix types

--- cardMakerFE/src/lib/fetchResource.ts
+++ cardMakerFE/src/lib/fetchResource.ts
@@ -1,12 +1,13 @@
 import { PUBLIC_BASE_API_URL } from '$env/static/public';
-import type { ErrorWithMessage } from './interfaces';
+import { getErrorMessage } from "./errors";
+
 
 
 export async function fetchCards() {
     try {
         return await fetchResource("cards");
     } catch (error) {
-        console.error(`Error fetching cards!: ${getErrorMessage(getErrorMessage(error))}`);
+        console.error(`Error fetching cards!: ${getErrorMessage(error)}`);
         return [];
     }
 }
@@ -51,31 +52,6 @@ async function fetchResource(resourceName: string) {
 }
 
 
-function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
-	return (
-		typeof error === 'object' &&
-		error !== null &&
-		'message' in error &&
-		typeof (error as Record<string, unknown>).message === 'string'
-	)
-}
 
-
-function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
-	if (isErrorWithMessage(maybeError)) return maybeError
-
-	try {
-		return new Error(JSON.stringify(maybeError))
-	} catch {
-		// fallback in case there's an error stringifying the maybeError
-		// like with circular references for example.
-		return new Error(String(maybeError))
-	}
-}
-
-
-function getErrorMessage(error: unknown) {
-	return toErrorWithMessage(error).message
-}
 
 
--- cardMakerFE/src/routes/card/list/+page.svelte
+++ cardMakerFE/src/routes/card/list/+page.svelte
@@ -1,82 +1,81 @@
 <script lang="ts">
-    import FilterDropdown from "$lib/components/FilterDropdown.svelte";
-	import TableRow from "$lib/components/TableRow.svelte";
-    import Card from '$lib/components/Card.svelte';
-    import type { Author, CardType, Tag } from "$lib/types";
-    import { fetchCards, fetchAuthors, fetchTypes, fetchTags } from "$lib/fetchResource";
-	import { onMount } from "svelte";
-	import FilterCheckbox from "$lib/components/FilterCheckbox.svelte";
-    import { api } from '$lib/stores/store';
+	import FilterDropdown from '$lib/components/FilterDropdown.svelte';
+	import TableRow from '$lib/components/TableRow.svelte';
+	import Card from '$lib/components/Card.svelte';
+	import type { Author, CardType, Tag } from '$lib/types';
+	import { fetchCards, fetchAuthors, fetchTypes, fetchTags } from '$lib/fetchResource';
+	import { onMount } from 'svelte';
+	import FilterCheckbox from '$lib/components/FilterCheckbox.svelte';
+	import { api } from '$lib/stores/store';
 
+	function getFilteredCards(
+		allCards: CardGet[] | [],
+		selectedAuthor: UserPublic | null,
+		selectedType: CardType | null,
+		activeTags: number[]
+	) {
+		let cards = allCards;
+		if (selectedAuthor != null) {
+			cards = cards.filter((card: CardGet) => {
+				return card.user_id === selectedAuthor.id;
+			});
+		}
+		if (selectedType != null) {
+			cards = cards.filter((card: CardGet) => {
+				return card.card_type_id === selectedType.id;
+			});
+		}
+		if (activeTags.length > 0) {
+			for (const tagName of activeTags) {
+				cards = cards.filter((card: CardGet) => {
+					for (const cardTag of card.tags) {
+						if (cardTag.name === tagName) {
+							return true;
+						}
+					}
+					return false;
+				});
+			}
+		}
+		return cards;
+	}
 
-    function getFilteredCards(allCards: Card[]|[], selectedAuthor: Author|null, selectedType: CardType|null, activeTags: number[]) {
-        let cards = allCards;
-        if (selectedAuthor != null) {
-            cards = cards.filter((card: Card) => {
-                return card.user_id === selectedAuthor.id;
-            });
-        }
-        if (selectedType != null) {
-            cards = cards.filter((card: Card) => {
-                return card.card_type_id === selectedType.id;
-            });
-        }
-        if (activeTags.length > 0) {
-            for (const tagName of activeTags) {
-                cards = cards.filter((card: Card) => {
-                    for (const cardTag of card.tags) {
-                        if (cardTag.name === tagName) {
-                            return true;
-                        }
-                    }
-                    return false;
-                })
-            }
-        }
-        return cards;
-    }
+	let allCards: CardGet[] | [] = [];
+	let authors: UserPublic[] | [] = [];
+	let types: CardType[] | [] = [];
+	let tags: Tag[] | [] = [];
+	let activeTags: number[] | [] = [];
 
+	onMount(async () => {
+		allCards = await $api.getCards();
+		authors = await $api.getUsers();
+		types = await $api.getCardTypes();
+		tags = await $api.getTags();
+	});
 
-    let allCards: Card[]|[] = [];
-    let authors: Author[]|[] = [];
-    let types: CardType[]|[] = [];
-    let tags: Tag[]|[] = [];
-    let activeTags: number[]|[] = [];
-    
-    onMount(async () => {
-        allCards = await $api.getCards();
-        authors = await $api.getUsers();
-        types = await $api.getCardTypes();
-        tags = await $api.getTags();
-    });
+	let selectedAuthor: UserPublic | null = null;
+	let selectedType: CardType | null = null;
 
-    let selectedAuthor: Author|null = null;
-    let selectedType: CardType|null = null;
-
-    let filteredCards: Card[] = allCards;
-    $: filteredCards = getFilteredCards(allCards, selectedAuthor, selectedType, activeTags);
+	let filteredCards: CardGet[] = allCards;
+	$: filteredCards = getFilteredCards(allCards, selectedAuthor, selectedType, activeTags);
 </script>
 
 <div class="card-list-body">
-    <div class="filters">
-        <FilterDropdown bind:selected={selectedAuthor} filterName="Autor" options={authors}/>
-        <FilterDropdown bind:selected={selectedType} filterName="Typ karty" options={types}/>
-        <FilterCheckbox bind:activeTags options={tags}/>
-    </div>
-    <div>
-        <table>
-            <tr>
-                <th>Karta</th>
-                <th>Autor</th>
-                <th>Typ</th>
-            </tr>
-            {#each filteredCards as card}
-                <TableRow
-                    card={card}
-                    authors={authors}
-                    types={types}
-                />
-            {/each}
-        </table>
-    </div>
+	<div class="filters">
+		<FilterDropdown bind:selected={selectedAuthor} filterName="Autor" options={authors} />
+		<FilterDropdown bind:selected={selectedType} filterName="Typ karty" options={types} />
+		<FilterCheckbox bind:activeTags options={tags} />
+	</div>
+	<div>
+		<table>
+			<tr>
+				<th>Karta</th>
+				<th>Autor</th>
+				<th>Typ</th>
+			</tr>
+			{#each filteredCards as card}
+				<TableRow {card} {authors} {types} />
+			{/each}
+		</table>
+	</div>
 </div>
