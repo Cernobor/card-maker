d0bc41dee5835caf9399ef9d3fe1e81820ac9878
pulcovamon <pulcovamon@gmail.com>
2024-07-31 23:03:06 +0200

    change message according to error code

--- cardMakerFE/src/routes/registration/+page.svelte
+++ cardMakerFE/src/routes/registration/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import HTTPError from '$lib/errors';
 	import type { UserCreate } from '$lib/interfaces';
 	import { api } from '$lib/stores/store';
 
@@ -27,10 +28,20 @@
 			.then(() => {
 				goto('/login');
 			})
-			.catch(() => {
-				wrongRegistration(
-					`Nelze se registrovat, buď máš špatný tajný klíč nebo už existuje uživatel ${user.username}.`
-				);
+			.catch((error) => {
+				if (error instanceof HTTPError) {
+					if (error.code === 401) {
+						wrongRegistration('Zadaný tajný klíč je nesprávný!');
+					} else if (error.code === 403) {
+						wrongRegistration(
+							`Uživatel ${user.username} už existuje, zvol si jiné uživatelské jméno.`
+						);
+					} else {
+						wrongRegistration('Registrace se nezdařila.');
+					}
+				} else {
+					wrongRegistration('Registrace se nezdařila.');
+				}
 			});
 	}
 </script>
