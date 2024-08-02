ce2d20722bcae3364344e2c5d45d4a1f4e6dada0
pulcovamon <pulcovamon@gmail.com>
2024-08-03 01:04:44 +0200

    change tags filter from checkboxes to labels

--- cardMakerFE/src/lib/components/FilterCheckbox.svelte
+++ cardMakerFE/src/lib/components/FilterCheckbox.svelte
@@ -1,37 +1,63 @@
 <script lang="ts">
+	/**
+	 * @todo refactor this component and TagSelector into one component
+	 */
 	import type { Tag } from '$lib/interfaces';
 	export let options: Tag[];
 	export let activeTags: Tag[];
 
-	function handleChange(event: Event):boolean | void {
+	function handleAddTag(event: Event) {
 		/**
-		 * Update selected tags array according to checked checkboxes.
+		 * Add tag to card tags if card does not contain it.
 		 */
-		const target = event.target as HTMLInputElement;
-		if (target.checked) {
-			activeTags = [...activeTags, { name: target.value }];
-		} else {
-			activeTags = activeTags.filter((tag) => {
-				return tag.name !== target.value;
-			});
+		const target = event.target as HTMLSelectElement;
+		if (target.value) {
+			if (!checkIfCardContainsTag(target.value)) {
+				activeTags = [...activeTags, { name: target.value }];
+			}
+			target.value = '';
 		}
+	}
+
+	function checkIfCardContainsTag(tag: string) {
+		/**
+		 * Compare given tag with card tags
+		 * and return true if card has tags else false
+		 */
+		for (const cardTag of activeTags) {
+			if (cardTag.name === tag) {
+				return true;
+			}
+		}
+		return false;
 	}
 </script>
 
 <div>
 	<h3 class="filter-title">Tagy</h3>
-	<div class="all-checkboxes-container">
-		{#each options as option}
-			<div class="checkbox-container">
-				<label for={option.name}>{option.name}</label>
-				<input
-					type="checkbox"
-					id={option.name}
-					class="checkbox"
-					value={option.name}
-					on:change={handleChange}
-				/>
-			</div>
-		{/each}
+	<div class="labels">
+		<ul class="labels">
+			{#each activeTags as tag}
+				<div class="tag-label">
+					<li>
+						{tag.name}
+						<button
+							class="tag-label-button"
+							on:click={() => {
+								activeTags = activeTags.filter((cardTag) => {
+									return cardTag.name !== tag.name;
+								});
+							}}>X</button
+						>
+					</li>
+				</div>
+			{/each}
+		</ul>
+		<select class="add-tag" on:change={handleAddTag}>
+			<option value=""></option>
+			{#each options as tag}
+				<option value={tag.name}>{tag.name}</option>
+			{/each}
+		</select>
 	</div>
 </div>
