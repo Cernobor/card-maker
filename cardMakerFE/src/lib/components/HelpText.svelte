bc42446d21b9a675c95a4bfa4f5ff338ec099a4e
Jakub <jakub.rutrle@firma.seznam.cz>
2024-08-03 15:32:36 +0200

    help for text creation

---
+++ cardMakerFE/src/lib/components/HelpText.svelte
@@ -0,0 +1,7 @@
+
+<div>
+    <p><b>tlustě:</b> &lt;b&gt;text&lt;/b&gt;</p>
+    <p><i>kurziva:</i> &lt;i&gt;text&lt;/i&gt;</p>
+    <p><u>podtrženě:</u> &lt;u&gt;text&lt;/u&gt;</p>
+    <p>slova moci - <span class="lingua-prima">A</span> : $uhurus</p>
+</div>
--- cardMakerFE/src/routes/+page.svelte
+++ cardMakerFE/src/routes/+page.svelte
@@ -1,8 +1,7 @@
+<script>
+    import  HelpText  from '$lib/components/HelpText.svelte';
+</script>
+
 <h1>Help</h1>
 
-
-
-<p><b>tlustě:</b> &lt;b&gt;text&lt;/b&gt;</p>
-<p><i>kurziva:</i> &lt;i&gt;text&lt;/i&gt;</p>
-<p><u>podtrženě:</u> &lt;u&gt;text&lt;/u&gt;</p>
-<p>slova moci - <span class="lingua-prima">A</span> : $uhurus</p>
+<HelpText />
