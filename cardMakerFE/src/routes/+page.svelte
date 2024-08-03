65822d093c8e7b3dfaa5b523029a6f32b6eaf7d4
Jakub <jakub.rutrle@firma.seznam.cz>
2024-08-03 15:13:59 +0200

    add basic help

--- cardMakerFE/src/routes/+page.svelte
+++ cardMakerFE/src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>home page</h1>
+<h1>Help</h1>
 
 
+
+<p><b>tlustě:</b> &lt;b&gt;text&lt;/b&gt;</p>
+<p><i>kurziva:</i> &lt;i&gt;text&lt;/i&gt;</p>
+<p><u>podtrženě:</u> &lt;u&gt;text&lt;/u&gt;</p>
+<p>slova moci - <span class="lingua-prima">A</span> : $uhurus</p>
