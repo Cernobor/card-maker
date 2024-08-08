68da5e038a1de42f703be2602e830281c4ffd88a
pulcovamon <pulcovamon@gmail.com>
2024-08-08 14:19:05 +0200

    update start script

--- api/start-api.sh
+++ api/start-api.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-while ! nc -z db 3306; do sleep 3; done
+echo "Waiting for MySQL to be available..."
+while ! nc -z db 3306; do
+  echo "MySQL not available yet, sleeping..."
+  sleep 3
+done
+echo "MySQL is up - executing application..."
 python main.py
