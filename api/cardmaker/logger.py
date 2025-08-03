08415e65db17132d687db2f71611194631d6042e
zegkljan <zegkljan@gmail.com>
2025-08-03 09:37:40 +0200

    Configurable logging

--- api/cardmaker/logger.py
+++ api/cardmaker/logger.py
@@ -4,6 +4,7 @@ Script for python logger setup.
 
 import logging
 import sys
+import os
 
 
 class Logger(object):
@@ -40,15 +41,23 @@ class Logger(object):
         Setup for python logger.
         All messages are logged into file 'cardmaker-api.log' and stdout.
         """
+        self.logger = logging.getLogger("CardmakerApi")
         formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 
-        file_handler = logging.FileHandler("cardmaker-api.log")
-        file_handler.setFormatter(formatter)
+        level = os.getenv("LOG_LEVEL")
+        if level is not None and level.lower() == "debug":
+            self.logger.setLevel(logging.DEBUG)
+        elif level is not None and level.lower() == "info":
+            self.logger.setLevel(logging.INFO)
+        else:
+            self.logger.setLevel(logging.WARNING)
 
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
-
-        self.logger = logging.getLogger("CardmakerApi")
-        self.logger.setLevel(logging.WARNING)
-        self.logger.addHandler(file_handler)
         self.logger.addHandler(stream_handler)
+
+        logfile = os.getenv("LOG_LOGFILE")
+        if logfile is not None and logfile != "":
+            file_handler = logging.FileHandler(logfile)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
